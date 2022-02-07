6f6abdce25870dba16aaf342f2e44388aa161d71
bayu07750 <bayu07750@gmail.com>
2022-02-07 11:27:49 +0700

    current location always null

--- location/src/main/java/com/bayu/location/ui/CurrentLocationFragment.kt
+++ location/src/main/java/com/bayu/location/ui/CurrentLocationFragment.kt
@@ -1,16 +1,67 @@
 package com.bayu.location.ui
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.location.Geocoder
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.bayu.location.BuildConfig
 import com.bayu.location.databinding.FragmentCurrentLocationBinding
+import com.bayu.location.extension.hasPermission
+import com.bayu.location.extension.shouldShowRationale
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.material.snackbar.Snackbar
 
 class CurrentLocationFragment : Fragment() {
 
     private var _binding: FragmentCurrentLocationBinding? = null
     private val binding get() = _binding!!
+
+    private val fusedLocationProviderClient by lazy {
+        LocationServices.getFusedLocationProviderClient(requireContext())
+    }
+
+    private var cancellationTokenSource = CancellationTokenSource()
+
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Snackbar.make(
+                binding.root,
+                "Permission Granted",
+                Snackbar.LENGTH_SHORT
+            ).show()
+        } else {
+            Snackbar.make(
+                binding.root,
+                "Permission Danied",
+                Snackbar.LENGTH_SHORT
+            )
+                .setAction("Go Setting") {
+                    val uri = Uri.fromParts(
+                        "package",
+                        BuildConfig.APPLICATION_ID,
+                        null
+                    )
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                        data = uri
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    }
+                    startActivity(intent)
+                }
+                .show()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +75,61 @@ class CurrentLocationFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnLocation.setOnClickListener {
-
+            if (!requireContext().hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                requestPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+            } else {
+                getCurrentLocation()
+            }
         }
+    }
+
+    private fun requestPermission(permission: String) {
+        val launchPermission = {
+            permissionLauncher.launch(permission)
+        }
+        if (requireActivity().shouldShowRationale(permission)) {
+            Snackbar.make(
+                binding.root,
+                "Kami butuh lokasi anda untuk menggunakan layanan ini",
+                Snackbar.LENGTH_SHORT
+            )
+                .setAction("Iya") {
+                    launchPermission()
+                }
+                .show()
+        } else {
+            launchPermission()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getCurrentLocation() {
+        val currentLocationTask = fusedLocationProviderClient.getCurrentLocation(
+            LocationRequest.PRIORITY_HIGH_ACCURACY,
+            cancellationTokenSource.token
+        )
+
+        currentLocationTask.addOnCompleteListener {
+            val result = if (it.isSuccessful && it.result != null) {
+                val location = it.result
+                val geocoder = Geocoder(requireContext()).getFromLocation(
+                    location.latitude,
+                    location.longitude,
+                    1
+                )
+                val address = geocoder.first()
+                "Berhasil mendapatkan Lokasi: ${address.countryName}/${address.adminArea}/${address.subAdminArea}/${address.locality}/${address.subLocality}/${address.thoroughfare}/${address.subThoroughfare}"
+            } else {
+                "Gagal mendapatkan Lokasi: ${it.exception?.message}"
+            }
+
+            binding.tvTitlePage.text = result
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        cancellationTokenSource.cancel()
     }
 
     override fun onDestroyView() {
